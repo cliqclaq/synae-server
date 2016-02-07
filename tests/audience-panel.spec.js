e63999f9f63c60c702847bfee0875be3bfa0036f
theoperatore <theoperatore@gmail.com>
2016-02-06 21:32:24 -0500

    initial testing of audience panel

---
+++ tests/audience-panel.spec.js
@@ -0,0 +1,50 @@
+import test from 'tape';
+import sinon from 'sinon';
+
+import React from 'react';
+import { createRenderer } from 'react-addons-test-utils';
+import AudiencePanel from '../frontend/components/audience-panel.jsx';
+import GroupChooser from '../frontend/components/group-chooser.jsx';
+
+test('<AudiencePanel />: no audio context', t => {
+  let renderer = createRenderer();
+  let noop = () => {};
+
+  renderer.render(<AudiencePanel rconnected={noop} rrecv={noop} rsend={noop} rid='test' />);
+  let output = renderer.getRenderOutput();
+
+  t.equals(output.props.children.type, 'button', 'renders a button to kick audio');
+  t.end();
+});
+
+test('<AudiencePanel />: has audio context', t => {
+  let renderer = createRenderer();
+  let noop = () => {};
+
+  let expectedMessage = <div className='audience'><div><h1 style={{ textAlign: 'center' }}>{'Waiting for Conductor...'}</h1></div></div>;
+  let mockedAudioContext = {};
+
+  renderer.render(<AudiencePanel actx={mockedAudioContext} rconnected={noop} rrecv={noop} rsend={noop} rid='test'/>);
+  let output = renderer.getRenderOutput();
+
+  t.deepEquals(output, expectedMessage, 'renders a waiting message');
+  t.end();
+});
+
+test('AudiencePanel />: has data but no group', t => {
+  let renderer = createRenderer();
+  let noop = () => {};
+
+  let rrecv = sinon.spy();
+  let mockedAudioContext = {};
+
+  renderer.render(<AudiencePanel actx={mockedAudioContext} rconnected={noop} rrecv={rrecv} rsend={noop} rid='test'/>);
+
+  // simulate response for world state
+  rrecv.getCall(0).args[0]('/world-state', ['{ "groups": [] }']);
+
+  let component = renderer.getRenderOutput();
+
+  t.equals(component.props.children.type, GroupChooser, 'renders a group chooser');
+  t.end();
+});
--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -2,28 +2,27 @@
 
 import React from 'react';
 import ReactTestUtils from 'react-addons-test-utils';
-import tape from 'tape';
+import test from 'tape';
 
 const renderer = ReactTestUtils.createRenderer();
-
 
 const Child1 = <h1>Some Content</h1>;
 const Child2 = <h2>A second child!</h2>;
 const Example = class extends React.Component {
   displayName = 'Example';
-  render() {
+  render () {
     return <div className='hello-world'>{Child1}{Child2}</div>;
   }
-}
+};
 
-tape('render an example component', t => {
+test.skip('<Example />', t => {
   t.plan(4);
 
   renderer.render(<Example />);
 
   let output = renderer.getRenderOutput();
-  t.equal(output.type, 'div');
-  t.equal(output.props.children.length, 2);
-  t.equal(output.props.children[0], Child1);
-  t.equal(output.props.children[1], Child2);
-})
+  t.equal(output.type, 'div', 'renders a div');
+  t.equal(output.props.children.length, 2, 'has the correct number of children');
+  t.equal(output.props.children[0], Child1, 'renders the first child');
+  t.equal(output.props.children[1], Child2, 'renders the second child');
+});
