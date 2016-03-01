7ea5c984a913d33c539f8f951e76cbf0b0c84108
Andrew Petersen <kirbysayshi@gmail.com>
2016-03-01 02:43:50 -0500

    initial fiberlamp proxy passthrough
    To test:
    
    $ node bin/send -p 9967 /fiberlamp blah blah
    
    Not sure it's actually forwarded, but the server at least receives
    it...
    
    Still need to maybe write some tests for this.

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 
 require('localenv');
 
+const osc = require('node-osc');
 const createRhizome = require('./rhizome-server-repack');
+const RhizomeWSClient = require('rhizome-server').websockets.Client;
 const cluster = require('cluster');
 const numCpus = require('os').cpus().length;
 const debug = require('debug');
@@ -18,7 +20,7 @@ const HTTP_PORT = Number(process.env.HTTP_PORT);
 // returns a simple object to start the rhizome servers
 //
 ///////////////////////////////////////////////////////////////////////////////
-const rhizome = createRhizome({
+const servers = createRhizome({
   wssPort: Number(process.env.WSS_PORT), // should be different than http port
   oscPort: Number(process.env.OSC_PORT),
   outputDir: __dirname + '/tmp',
@@ -38,10 +40,35 @@ if (cluster.isMaster) {
     });
   }
 
-  rhizome.start((err, serverConfig) => {
+  // TODO: not really sure it's worth having rhizome-server-repack/index
+  // when most of the config is neede out here too.
+  servers.start((err, serverConfig) => {
     if (err) throw err;
 
     sl(`${process.pid}: server ports: wss ${serverConfig.wss}, osc ${serverConfig.osc}`);
+
+    const fiberlamp = new osc.Client(
+      process.env.FIBERLAMP_ADDRESS,
+      parseInt(process.env.FIBERLAMP_PORT, 10)
+    );
+
+    const client = new RhizomeWSClient({
+      hostname: '127.0.0.1', // TODO: make this configurable?
+      port: Number(process.env.WSS_PORT),
+    });
+
+    client.start(() => sl('fiberlamp proxy client connected'));
+
+    client.on('connected', () => {
+      client.send('/sys/subscribe', ['/fiberlamp']);
+    });
+
+    client.on('message', (address, args) => {
+      if (address !== '/fiberlamp') return;
+      const msg = new osc.Message(...[address, ...args]);
+      sl('sending %o', msg);
+      fiberlamp.send(msg);
+    });
   });
 }
 
